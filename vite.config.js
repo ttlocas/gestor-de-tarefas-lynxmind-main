a2a22f29f5868188c9b5295f19494d461eadb7a7
ttlocas <tsm130109@gmail.com>
2026-02-12 16:16:32 +0000

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/gestor-de-tarefas-lynxmind/"
+  base: './', // <-- isso faz os caminhos funcionarem no Vercel
 });
